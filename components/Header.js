8fdecd9a47ac7698c956c30b55cfc79ad0affa5d
abinash <subediabinas@gmail.com>
2023-05-27 16:35:22 +0545

    created ecommerce app from nextjs

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+const Header = () => {
+  return (
+    <>
+        <div className="">
+            <div className="">
+                <div className="Logo">
+                    <h3><Link href={'/'}>EcomWeb</Link></h3>    
+                </div>    
+            </div>    
+        </div>   
+    </>
+  )
+}
+
+export default Header
